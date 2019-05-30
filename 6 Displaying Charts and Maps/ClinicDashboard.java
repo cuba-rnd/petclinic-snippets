9dd311a9aa40cbeee8a2f8526b780acd4ca35186
Andrey Belyaev <belyaev.andrey@gmail.com>
2019-05-30 16:21:11 +0300

    Added dashboard UI

---
+++ 6 Displaying Charts and Maps/ClinicDashboard.java
@@ -0,0 +1,10 @@
+package com.company.clinic.web.dashboard;
+
+import com.haulmont.cuba.gui.screen.Screen;
+import com.haulmont.cuba.gui.screen.UiController;
+import com.haulmont.cuba.gui.screen.UiDescriptor;
+
+@UiController("clinic_ClinicDashboard")
+@UiDescriptor("clinic-dashboard.xml")
+public class ClinicDashboard extends Screen {
+}
